8ed0617066fa32a98bb86998e6cb9837c22bfd2d
vladislavzyatkovskiy <zyatkovskyy@gmail.com>
2024-09-21 19:39:17 +0500

    Initial commit

---
+++ src/main/java/com/vladzyatkovski/task_for_solva/MicroserviceForSolvaApplication.java
@@ -0,0 +1,13 @@
+package com.vladzyatkovski.task_for_solva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceForSolvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceForSolvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vladzyatkovski/task_for_solva/MicroserviceForSolvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladzyatkovski.task_for_solva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceForSolvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
